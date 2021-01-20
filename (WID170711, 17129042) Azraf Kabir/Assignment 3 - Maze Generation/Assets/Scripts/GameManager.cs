bbc78f887adc1fb40e2bbf0d0ad24b39dbc7b707
Govardhan Padmanabhan <39567171+GovardhanPadmanabhan@users.noreply.github.com>
2021-01-20 21:00:35 +0800

    Added files for the mini project

---
+++ (WID170711, 17129042) Azraf Kabir/Assignment 3 - Maze Generation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+
+	public Player playerPrefab;
+
+	private Maze mazeInstance;
+
+	private Player playerInstance;
+
+	private void Start () {
+		StartCoroutine(BeginGame());
+	}
+	
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}
+		
+	}
+
+	private IEnumerator BeginGame () {
+		Camera.main.clearFlags = CameraClearFlags.Skybox;
+		Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		IntVector2 startCoordinate = mazeInstance.RandomCoordinates;
+		yield return StartCoroutine(mazeInstance.Generate(startCoordinate));
+		playerInstance = Instantiate(playerPrefab) as Player;
+		playerInstance.SetLocation(mazeInstance.GetCell(startCoordinate));
+		Camera.main.clearFlags = CameraClearFlags.Depth;
+		Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+		
+		write_eval_csv(mazeInstance.GetDeadEnds());
+	}
+
+	private void RestartGame () {
+		StopAllCoroutines();
+		Destroy(mazeInstance.gameObject);
+		if (playerInstance != null) {
+			Destroy(playerInstance.gameObject);
+		}
+		StartCoroutine(BeginGame());
+	}
+
+	public void write_eval_csv (int total_dead_ends) {
+        var csv = new StringBuilder();
+        var results = string.Format("{0}", total_dead_ends);
+        csv.AppendLine(results);
+
+        string path = @"Evaluation Results.csv";
+
+        File.WriteAllText(path, csv.ToString());
+    }
+}
---
+++ Mini Project/README.md
@@ -0,0 +1,37 @@
+# 2048 AI
+This project is the submission for the mini-project for WID3009: AI Game Programming
+
+This project plays the game 2048 using an AI algorithm (Min-Max algorithm with a-b pruning)
+
+
+# 2048
+A single-player sliding tile puzzle game designed by Gabriele Cirulli.
+
+It's a 4x4 grid with 2-power valued tiles populating it. The player needs to swipe the tiles in a possible direction, and try to combine tiles to get 2048 or greater.
+(think of it as candy crush for math fanatics)
+
+It's available on various platforms. You can play the original browser version [here](https://play2048.co/).
+
+
+# Requirements
+
+*   Python Notebook
+*   Selenium Library
+*   Chrome Browser
+*   Chrome Web Driver
+*   Internet Connection
+
+For Selenium to run the browser, the Chrome web driver is needed, which can be downloaded from [here](https://chromedriver.chromium.org/downloads).
+
+***Note!!***    Need to place the downloaded `.exe` file in the same directory as that of the notebooks. Else, need to change the respective paths in the notebook.
+
+
+# Execution
+
+***Important!!*** Before running the notebooks, make sure the path to the web driver is correct.
+
+
+To run the program, just have to run the entire notebooks
+
+`2048 AI (with a-b pruning).ipynb` notebook implements the minimax algorithm for 2048 with a-b pruning
+`2048 AI (without a-b pruning).ipynb` notebook implements the minimax algorithm for 2048 without a-b pruning
